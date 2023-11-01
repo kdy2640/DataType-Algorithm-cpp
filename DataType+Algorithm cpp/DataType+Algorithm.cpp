c94c4777b9544ff608c900e0952526a45f68275a
harumil <rlaehduq2640@gmail.com>
2023-11-01 18:37:47 +0900

    Feat : add ArithmeticExpressionEvaluator class and main

--- DataType+Algorithm cpp/DataType+Algorithm.cpp
+++ DataType+Algorithm cpp/DataType+Algorithm.cpp
@@ -1,30 +1,13 @@
 ﻿#include<iostream>
 #include<fstream>
-#include"OurDeque.cpp"
+#include "ArithmeticExpressionEvaluator.h"
 using namespace std;
 using namespace DataType;
 
 int main()
 {
-	OurDeque<char> temp;
-	for (int i = 0 ;i < 20; i++)
-	{
-		temp.push_front(65+i);
-	}
-	temp.show_contents();
-
-	temp.push_back(88);
-	temp.push_back(89);
-	temp.push_back(90);
-
-	temp.show_contents();
-
-	temp.pop_front();
-	temp.pop_back();
-
-	temp.show_contents();
-
-	cout << temp.front() << temp.back() << endl;
+	AEE evaluator;
+	cout << evaluator.read_expression();
 
 }
 
